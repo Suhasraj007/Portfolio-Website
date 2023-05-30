5f6be085b91a635e7e48b9ec9611d58e3c0dbbfd
Suhasraj007 <20etcs002137@msruas.ac.in>
2023-05-30 18:12:59 +0530

    scrolling animation begin

--- js/main.js
+++ js/main.js
@@ -1,20 +1,60 @@
-document.addEventListener('readystatechange',(event)=>{
-    if (event.target.readyState === 'complete') {
-        console.log('readyState: complete');
-        initApp();
-    }
-})
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    console.log("readyState: complete");
+    initApp();
+  }
+});
 
 const initApp = () => {
-    const project = document.querySelector('#projects');
-    const view = document.querySelector('.header__nav--extender')
-    project.addEventListener('mouseover', (event) =>{
-        view.style.display = 'block';
-    } )
-    view.addEventListener('mouseover', (event) =>{
-        view.style.display = 'block'; 
-    } )
-    view.addEventListener('mouseout', (event) =>{
-        view.style.display = 'none';
-    } )
+
+  const project = document.querySelector("#projects");
+  const view = document.querySelector(".header__project--extender");
+
+  project.addEventListener("mouseover", (event) => {
+    view.style.display = "block";
+  });
+
+  view.addEventListener("mouseover", (event) => {
+    view.style.display = "block";
+  });
+
+  view.addEventListener("mouseout", (event) => {
+    view.style.display = "none";
+  });
+
+  project.addEventListener("mouseout", (event) => {
+      view.style.display = "none";
+  });
+
+  let lastKnownScrollPosition = 0;
+
+  function doSomething() {
+    // Do something with the scroll position
+    nav = document.querySelector('.header__nav--container');
+    nav.style.background = "#121426";
+    nav.style.display = "flex";
+    nav.style.flexFlow = "row nowrap";
+    nav.style.justifyContent = "space-between";
+    nav.style.alignItems = "center";
+  }
+
+  function revert(){
+    nav = document.querySelector('.header__nav--container');
+    nav.style.background = "none";
+    nav.style.flexFlow = "column nowrap";
+  }
+
+  document.addEventListener("scroll", (event) => {
+    lastKnownScrollPosition = window.scrollY;
+  
+    if (lastKnownScrollPosition > 730) {
+        doSomething();
+    } else{
+        revert();
+    }
+
+  });
+
 }
+
+
